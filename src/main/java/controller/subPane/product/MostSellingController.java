2924d795476c9e12641982d3301ace60bddff0f6
Yashan97 <s.d.y.anupama@gmail.com>
2026-01-22 22:20:19 +0530

    create most Selling product form

---
+++ src/main/java/controller/subPane/product/MostSellingController.java
@@ -0,0 +1,5 @@
+package controller.subPane.product;
+
+public class MostSellingController {
+
+}
--- src/main/java/controller/subPane/product/ProductFormController.java
+++ src/main/java/controller/subPane/product/ProductFormController.java
@@ -57,9 +57,31 @@ public class ProductFormController implements Initializable {
     }
 
     public void btnAddCategory(ActionEvent event) {
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/subPanes/product/addCategory.fxml"));
+            AnchorPane.setTopAnchor(pane, 0.0);
+            AnchorPane.setBottomAnchor(pane, 0.0);
+            AnchorPane.setLeftAnchor(pane, 0.0);
+            AnchorPane.setRightAnchor(pane, 0.0);
+            ancProduct.getChildren().clear();
+            ancProduct.getChildren().add(pane);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void btnMostSellingProduct(ActionEvent event) {
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/subPanes/product/mostSellingProduct.fxml"));
+            AnchorPane.setTopAnchor(pane, 0.0);
+            AnchorPane.setBottomAnchor(pane, 0.0);
+            AnchorPane.setLeftAnchor(pane, 0.0);
+            AnchorPane.setRightAnchor(pane, 0.0);
+            ancProduct.getChildren().clear();
+            ancProduct.getChildren().add(pane);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void btnProductReport(ActionEvent event) {
